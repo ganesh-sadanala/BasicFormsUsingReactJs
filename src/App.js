83f25a9b259b80b1944bbe4eeadd3549e3a69352
ganesh-sadanala <ganesh8374387106@gmail.com>
2020-09-18 11:12:17 +0530

    Add useState Hook and define state

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import "./index.css";
 
 export default function App() {
+  const [values, setValues] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+  });
   return (
     <div className="form-container">
       <form class="register-form">
@@ -12,6 +17,7 @@ export default function App() {
           type="text"
           placeholder="First Name"
           name="firstName"
+          value={values.firstName}
         />
         <span id="first-name-error">Please enter a first name</span>
         <input
@@ -20,6 +26,7 @@ export default function App() {
           type="text"
           placeholder="Last Name"
           name="lastName"
+          value={values.lastName}
         />
         <span id="last-name-error">Please enter a last name</span>
         <input
@@ -28,6 +35,7 @@ export default function App() {
           type="text"
           placeholder="Email"
           name="email"
+          value={values.email}
         />
         <span id="email-error">Please enter an email address</span>
         <button class="form-field" type="submit">
